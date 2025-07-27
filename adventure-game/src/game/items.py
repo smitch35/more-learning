f3bf99d6625a1f1672f4ea37ab33b9a6146eab21
Sean Mitchuson <sean.mitchuson@gmail.com>
2025-07-27 08:52:25 -0400

    itemsrefactor

--- adventure-game/src/game/items.py
+++ adventure-game/src/game/items.py
@@ -1,9 +1,13 @@
-possible_items = ["sword", "shield", "potion", "map", "key", "letter", "torch","hi-potion"]
+possible_items = {
+    "equipment": ["sword", "shield"],
+    "consumables": ["potion", "hi-potion"],
+    "story_items": ["map", "key", "letter", "torch"]
+}
 
 
 item_descriptions = {
-    "sword": {"desc": "A sharp blade, useful for fighting enemies.","health": 2 },
-    "shield": {"desc": "A sturdy shield to protect you from attacks.", "health": 4},
+    "sword": {"desc": "A sharp blade, useful for fighting enemies."},
+    "shield": {"desc": "A sturdy shield to protect you from attacks."},
     "potion": {"desc": "A mysterious liquid that restores health."},
     "map": {"desc": "A map showing the surrounding area."},
     "hi-potion": {"desc": "Larger than the initial potion"},
@@ -13,5 +17,9 @@ item_descriptions = {
 }
 
 item_effects = {
-    "potion": {"health": 10}
+    "potion": {"health": 10},
+    "hi-potion": {"health": 20},
+    "sword": {"strength": 2},
+    "shield": {"health": 5}
+
 }
